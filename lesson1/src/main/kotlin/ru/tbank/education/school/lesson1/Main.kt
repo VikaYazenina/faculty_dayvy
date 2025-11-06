56411b067390efeb4a50dfd36609e8fd1504d962
vikayazenina <vikayazenina@list.ru>
2025-11-06 10:13:30 +0300

    Initial project setup with lesson1 module

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
@@ -1,4 +1,13 @@
 package ru.tbank.education.school.lesson1
 
 fun main() {
+    print("Hello world")
+    println("Hello world")
+    val c=3
+    val d="hello $c people"
+    println(d)
+    var a=2
+    println(a+1)
+    val b: Short?=null
+    println(b)
 }
