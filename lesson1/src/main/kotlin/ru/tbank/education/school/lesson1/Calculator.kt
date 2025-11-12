ec6dc2bb8a4232785db28d942d3db550dbd6557e
vikayazenina <vikayazenina@list.ru>
2025-11-12 23:56:27 +0300

    Initial project setup with lesson1 module

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
@@ -4,7 +4,14 @@ package ru.tbank.education.school.lesson1
  * Метод для вычисления простых арифметических операций.
  */
 fun calculate(a: Double, b: Double, operation: OperationType): Double? {
-    TODO()
+    when(operation){
+        OperationType.ADD-> a+b
+        OperationType.SUBTRACT  -> a-b
+        OperationType.MULTIPLY -> a*b
+        OperationType.DIVIDE -> if (b.toInt()!=0) a/b else null
+        else -> a+b
+    }.toString()
+    return
 }
 
 /**
@@ -14,5 +21,7 @@ fun calculate(a: Double, b: Double, operation: OperationType): Double? {
  */
 @Suppress("ReturnCount")
 fun String.calculate(): Double? {
-    TODO()
+    TODO(
+
+    )
 }
