84a37a97782464627f1f3d5e5eb8973d45cfe983
vikayazenina@list.ru <vikayazenina@list.ru>
2025-11-13 00:13:47 +0300

    Merge remote-tracking branch 'origin/master'

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
@@ -1,22 +1,18 @@
 package ru.tbank.education.school.lesson1
+enum class OperationType{
+    ADD, SUBTRACT, MULTIPLY, DIVIDE
+}
 fun calculate(a: Double, b: Double, operation: OperationType): Double? {
-    when(operation){
+    return when(operation){
         OperationType.ADD-> a+b
         OperationType.SUBTRACT  -> a-b
         OperationType.MULTIPLY -> a*b
-        OperationType.DIVIDE -> if (b.toInt()!=0) a/b else null
+        OperationType.DIVIDE -> if (b!=0.0) a/b else null
         else -> a+b
-    }.toString()
-    return
+    }
 }
-
-fun main() {
-    print("Hello world")
-    println("Hello world")
-    val c=3
-    val d="hello $c people"
-    println(d)
-    var a=2
-    println(a+1)
+fun main(){
 
 }
+
+
