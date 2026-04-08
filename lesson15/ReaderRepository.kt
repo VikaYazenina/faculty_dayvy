6dac690251d5a75a9fd309e51b430ce0c39ff2e4
VikaYazenina <vikayazenina@list.ru>
2026-04-08 23:51:57 +0300

    Update ReaderRepository.kt

--- lesson15/ReaderRepository.kt
+++ lesson15/ReaderRepository.kt
@@ -3,7 +3,4 @@ package com.example.library.repository
 import com.example.library.entity.Reader
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface ReaderRepository : JpaRepository<Reader, Long> {
-    @Query("SELECT r FROM ReaderEntity r LEFT JOIN FETCH r.books")
-    fun findAllWithBooks(): List<ReaderEntity>
-}
+interface ReaderRepository : JpaRepository<Reader, Long> 
