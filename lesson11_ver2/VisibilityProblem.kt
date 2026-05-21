6df5884eabc22953dd988590ac39a8dfd4e60b7e
VikaYazenina <vikayazenina@list.ru>
2026-05-21 21:50:31 +0300

    Create VisibilityProblem.kt

---
+++ lesson11_ver2/VisibilityProblem.kt
@@ -0,0 +1,29 @@
+class VisibilityProblem {
+    @Volatile
+    private var running = true
+
+    fun startWriter(): Thread {
+        return Thread {
+            
+            repeat(100) {
+                Thread.sleep(10)
+                Thread.yield()
+            }
+
+            running = false
+            println("Writer: установил running = false")
+        }
+    }
+
+    fun startReader(): Thread {
+        return Thread {
+            println("Reader: начал работу (ждет running = false)")
+
+            while (running) {
+             
+            }
+
+            println("Reader: завершил работу (увидел running = false)")
+        }
+    }
+}
